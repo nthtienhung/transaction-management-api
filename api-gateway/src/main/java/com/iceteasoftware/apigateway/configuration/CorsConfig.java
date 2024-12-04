ec3246efebaa46f3e4aa1394bebfdaa025a2ab9c
Vũ Ngọc Tuệ Minh <minhvnt@iceteasoftware.com>
2024-12-04 15:12:04 +0700

    fix bug

--- api-gateway/src/main/java/com/iceteasoftware/apigateway/configuration/CorsConfig.java
+++ api-gateway/src/main/java/com/iceteasoftware/apigateway/configuration/CorsConfig.java
@@ -4,6 +4,9 @@ import com.iceteasoftware.apigateway.filter.JwtTokenInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -26,11 +29,17 @@ public class CorsConfig implements WebMvcConfigurer {
         System.out.println("JwtTokenInterceptor đã được đăng ký!");
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PATCH", "DELETE");
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000"); // Cho phép origin của frontend
+        config.addAllowedMethod("*"); // Cho phép tất cả các phương thức (GET, POST, PUT, DELETE,...)
+        config.addAllowedHeader("*"); // Cho phép tất cả các header
+        config.setAllowCredentials(true); // Cho phép gửi cookie
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 }
--- iam-service/src/main/java/com/iceteasoftware/iam/configuration/security/SecurityConfiguration.java
+++ iam-service/src/main/java/com/iceteasoftware/iam/configuration/security/SecurityConfiguration.java
@@ -24,7 +24,7 @@ import java.util.List;
 public class SecurityConfiguration {
     private final JWTCookieFilter jwtCookieFilter;
     private final String[] WHITE_LIST ={
-            "/v3/api-docs/**","/swagger/**","/swagger-ui/**","/login", "/register/**", "/forgot-password/**"};
+            "/v3/api-docs/**","/swagger/**","/swagger-ui/**","/login", "/register/**", "/forgot-password/**", "/change-password/**"};
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
