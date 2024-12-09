b3504e0b0e54184cf40a41cdb5bc65953725f593
Nguyenhoang164 <kamito062004@gmail.com>
2024-12-09 11:22:07 +0700

    save

--- user-service/src/main/java/com/iceteasoftware/user/controller/UserController.java
+++ user-service/src/main/java/com/iceteasoftware/user/controller/UserController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin("*")
 public class UserController {
 
     private final UserService userService;
