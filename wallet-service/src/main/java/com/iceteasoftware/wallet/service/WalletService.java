041045bd5bea111c7556f0c93426f89a36e71187
Nguyenhoang164 <kamito062004@gmail.com>
2024-12-11 14:20:43 +0700

    create transaction manager admin and refresh token account

--- wallet-service/src/main/java/com/iceteasoftware/wallet/service/WalletService.java
+++ wallet-service/src/main/java/com/iceteasoftware/wallet/service/WalletService.java
@@ -15,4 +15,5 @@ public interface WalletService {
     WalletResponse getWalletByCode(String walletCode);
 
     void updateWalletBalance(String walletCode, Long amount);
+    WalletResponse getWalletByUserId(String userId);
 }
